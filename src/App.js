f7223f97828949946e7586f17993dd365f4320ef
krishnau2 <krishnau2@gmail.com>
2019-07-23 01:01:08 +0530

    Added react-router-dom for handling navigation.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import Summary from './components/Summary'
+import Summary from "./components/Summary";
+import BankAccounts from "./components/BankAccounts";
+import CashAccounts from "./components/CashAccounts";
+import Budget from "./components/Budget";
+import Reports from "./components/Reports";
+import Settings from "./components/Settings";
 
 function App() {
   return (
     <div className="App">
-      <Summary />
+      <Router>
+        <div>
+          <Route exact path="/" component={Summary} />
+          <Route path="/bank_accounts" component={BankAccounts} />
+          <Route path="/cash_accounts" component={CashAccounts} />
+          <Route path="/budget" component={Budget} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/settings" component={Settings} />
+        </div>
+      </Router>
     </div>
   );
 }
